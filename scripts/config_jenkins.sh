b278b796c55b23ecd51870717d739ffedeaa2fa9
Vijay <isaradhi24@gmail.com>
2026-04-11 14:34:19 -0400

    Groovy script to tell Jenkins to create pipelien jobs and point them to files

--- scripts/config_jenkins.sh
+++ scripts/config_jenkins.sh
@@ -6,6 +6,8 @@
 
 set -ex
 
+# Define paths
+SYNCED_DIR="/var/lib/jenkins_home"
 JENKINS_HOME="/var/lib/jenkins"
 INIT_GROOVY="$JENKINS_HOME/init.groovy.d"
 
@@ -16,8 +18,8 @@ echo "Preparing init scripts directory..."
 sudo mkdir -p "$INIT_GROOVY"
 
 # Disable setup wizard
-echo "2.0" | sudo tee "$JENKINS_HOME/jenkins.install.UpgradeWizard.state"
-
+# echo "2.0" | sudo tee "$JENKINS_HOME/jenkins.install.UpgradeWizard.state"
+echo "2.0" | sudo tee "$JENKINS_HOME/jenkins.install.InstallUtil.lastExecVersion"
 # -----------------------------
 # Admin User and Security Setup
 # -----------------------------
@@ -35,6 +37,21 @@ instance.setSecurityRealm(hudsonRealm)
 // Set authorization strategy
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
+
+// --- Create Infra Pipeline Job ---
+def infraJobName = "k8s-bootstrap"
+def infraPipelineScript = new File("/var/lib/jenkins_home/pipelines/infra/k8s-bootstrap.Jenkinsfile").text
+def flowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition(infraPipelineScript, true)
+def job = instance.createProject(org.jenkinsci.plugins.workflow.job.WorkflowJob, infraJobName)
+job.setDefinition(flowDefinition)
+
+// --- Create Apps Pipeline Job ---
+def appJobName = "my-nginx-app"
+def appPipelineScript = new File("/var/lib/jenkins_home/pipelines/apps/my-nginx-app.Jenkinsfile").text
+def appFlowDefinition = new org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition(appPipelineScript, true)
+def appJob = instance.createProject(org.jenkinsci.plugins.workflow.job.WorkflowJob, appJobName)
+appJob.setDefinition(appFlowDefinition)
+
 
 instance.save()
 EOF
