714234ef2eaabe9a824d193f6bd92d9a164cd567
Vijay <isaradhi24@gmail.com>
2026-04-17 12:55:33 -0400

    GPG-key oweverwriting automaged

--- scripts/elk/install_elk.sh
+++ scripts/elk/install_elk.sh
@@ -3,7 +3,9 @@
 sudo apt-get update && sudo apt-get install -y apt-transport-https wget gpg openjdk-17-jre-headless
 
 # Add GPG Key and Repo
-wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+# wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -o /usr/share/keyrings/elasticsearch-keyring.gpg
+# Force overwrite if it exists
+curl -fsSL https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo gpg --dearmor -f -o /usr/share/keyrings/elasticsearch-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/elasticsearch-keyring.gpg] https://artifacts.elastic.co/packages/8.x/apt stable main" | sudo tee /etc/apt/sources.list.d/elastic-8.x.list
 
 sudo apt-get update
